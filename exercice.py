f6c5f0d9404a16e43feb0d51de7f2f99c38ae214
Thomas Demachy <tdemachy@free.fr>
2020-06-09 12:08:43 +0200

    Le resultat

--- exercice.py
+++ exercice.py
@@ -3,3 +3,25 @@
 # 2 - calculer la somme des données
 # 3 - Enregistrer le resultat dans un fichier result.txt
 # Bon courage
+
+f = open("data.txt")
+data_str = f.read()
+print(f"contenu {data_str}")
+
+data = [int(x) for x in data_str.split(',')]
+
+#Calculer la somme
+s = 0
+for x in data: s+= x
+
+# fermer le ficher en lecture
+f.close()
+
+#Ouvrir le fichier result.txt en ecriture
+f = open("result.txt", "wt")
+
+#Enregistrer la somme
+f.write(str(s))
+
+#Fermer le fichier
+f.close()
